2f28a6e7aa4e21ab7a3f1205174823896c88567a
Matt Wolenetz <wolenetz@chromium.org>
2021-04-14 15:09:37 -0700

    MSE-in-Workers: Add tests of detach concurrent with appendBuffer
    To help find faults like those fixed in crbug 1195441, this change adds
    a web test that is like the existing
    mediasource-worker-play-terminate-worker case, though instead it
    detaches the MediaSource from the element at various times before,
    during and after the worker is buffering in small pieces. Further
    refinement of the MediaSource readyState for the scenario where a
    concurrent "closing has begun" causes exception on attempt to use the
    closing MediaSource will be necessary to refine this new test further to
    not ignore so many exceptions in the worker thread; TODOs are included
    for that in the new test.
    
    BUG=1195441,878133
    TEST=mediasource-worker-detach-element.html, confirmed locally in a
    release build with DCHECKS enabled to reliably catch the issue in bug
    1195441 (with that bug's fix locally reverted for this check).
    
    Change-Id: I3e041d7b06db96d3d976593f20181d073fad1da1

---
+++ media-source/dedicated-worker/mediasource-worker-detach-element.js
@@ -0,0 +1,78 @@
+// This is similar to mediasource-worker-play.js, except that the buffering is
+// longer and done in tiny chunks to enable a better chance of the main thread
+// detaching the element while interesting buffering work is still occurring. To
+// assist the main thread understanding when the buffering has started already
+// or has completed already, we also perform extra messaging.
+importScripts("mediasource-worker-util.js");
+
+onmessage = function(evt) {
+  postMessage("Error: No message expected by Worker");
+};
+
+let util = new MediaSourceWorkerUtil();
+
+let sentStartedBufferingMessage = false;
+
+util.mediaSource.addEventListener("sourceopen", () => {
+  URL.revokeObjectURL(util.mediaSourceObjectUrl);
+  let sourceBuffer;
+  try {
+    sourceBuffer = util.mediaSource.addSourceBuffer(util.mediaMetadata.type);
+  }  catch(e) {
+    // Detachment may have already begun, so allow exception here.
+    // TODO(https://crbug.com/878133): Consider a distinct readyState for the case
+    // where exception occurs due to "Worker MediaSource attachment is closing".
+    // That would assist API users and narrow the exception handling here.
+    return;
+  }
+
+  sourceBuffer.onerror = (err) => {
+    postMessage("Error: " + err);
+  };
+  util.mediaLoadPromise.then(mediaData => bufferInto(sourceBuffer, mediaData, 100, 0),
+                             err => { postMessage("Error: " + err) } );
+}, { once : true });
+
+postMessage(util.mediaSourceObjectUrl);
+
+// Append increasingly large pieces at a time, starting/continuing at |position|.
+// This allows buffering the test media without timeout, but also with enough
+// operations to gain coverage on detachment concurrency with append.
+function bufferInto(sourceBuffer, mediaData, appendSize, position) {
+  if (position >= mediaData.byteLength) {
+    postMessage("Status: Finished Buffering");
+    try {
+      util.mediaSource.endOfStream();
+    }  catch(e) {
+      // Detachment may have already begun, so allow exception here.
+      // TODO(https://crbug.com/878133): Consider a distinct readyState for the case
+      // where exception occurs due to "Worker MediaSource attachment is closing".
+      // That would assist API users and narrow the exception handling here.
+      // FALL-THROUGH - return.
+    }
+    return;
+  }
+
+  var nextPosition = position + appendSize;
+  const pieceToAppend = mediaData.slice(position, nextPosition);
+  position = nextPosition;
+  appendSize += 100;
+
+  sourceBuffer.addEventListener("updateend", () => {
+    if (!sentStartedBufferingMessage) {
+      postMessage("Status: Started Buffering");
+      sentStartedBufferingMessage = true;
+    }
+    bufferInto(sourceBuffer, mediaData, appendSize, position);
+  }, { once : true });
+
+  try {
+    sourceBuffer.appendBuffer(pieceToAppend);
+  }  catch(e) {
+    // Detachment may have already begun, so allow exception here.
+    // TODO(https://crbug.com/878133): Consider a distinct readyState for the case
+    // where exception occurs due to "Worker MediaSource attachment is closing".
+    // That would assist API users and narrow the exception handling here.
+    // FALL-THROUGH - return.
+  }
+}
